a38f7db7fb635bf63d44a6024344cbda171bbb3c
Rajat Menon <22711531@student.uwa.edu.au>
2022-03-29 11:53:42 +0800

    Updating

--- lab1.js
+++ lab1.js
@@ -1,6 +1,13 @@
-function changeText(){
+function changeToCodes(){
     document.getElementById("AWD").innerHTML = "CITS3401";
     document.getElementById("DW").innerHTML = "CITS3403";
     document.getElementById("CN").innerHTML = "CITS3002";
     document.getElementById("OU").innerHTML = "SCIE1121";
+}
+
+function changeToNames(){
+    document.getElementById("AWD").innerHTML = "Agile Web Development";
+    document.getElementById("DW").innerHTML = "Data Warehousing";
+    document.getElementById("CN").innerHTML = "Computer Networks";
+    document.getElementById("OU").innerHTML = "Our Universe";
 }
