7359c5b9308c41dbceb8a1f79293cc43269eedb8
Rajat Menon <22711531@student.uwa.edu.au>
2022-03-29 11:42:35 +0800

    Updating

---
+++ lab1.js
@@ -0,0 +1,6 @@
+function changeText(){
+    document.getElementById("AWD").innerHTML = "CITS3401";
+    document.getElementById("DW").innerHTML = "CITS3403";
+    document.getElementById("CN").innerHTML = "CITS3002";
+    document.getElementById("OU").innerHTML = "SCIE1121";
+}
